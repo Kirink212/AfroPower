23ceff83fa6f1da3eedd1d44876d76d780784bad
lfbicalho <lfernandotexbicalho@hotmail.com>
2017-02-08 17:14:41 -0200

    Committing the game's GDD placeholder and the Unity project started to test the group's Unity ability.

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float velocity_x;
+	public float velocity_y;
+	public float jump_height;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D)) 
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (velocity_x, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		else if (Input.GetKey (KeyCode.LeftArrow) || Input.GetKey (KeyCode.A)) 
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (-velocity_x, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKey (KeyCode.W)) 
+		{
+			GetComponent<Rigidbody2D>().velocity = new Vector2 (GetComponent<Rigidbody2D>().velocity.x, jump_height);
+		}
+	}
+}
